b3b3ed68290695f19d655b7d96c6911faf7e5098
Jean Ribeiro Alexandre <jean.alexandre.rs@gmail.com>
2019-10-10 10:11:37 -0300

    Implemented hot loading.
    Added @hot-loader/react-dom patch using yarn name resolution

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -1,15 +1,16 @@
+import { hot } from 'react-hot-loader/root';
 import React from 'react';
-import {  Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 
 import routes from '../constants/routes';
 import Home from './Home';
 
-export default function App(): JSX.Element {
-    return (
-        <Router>
-            <Switch>
-                <Route path={routes.HOME} component={Home} />
-            </Switch>
-        </Router>
-    );
-}
+const App = (): JSX.Element => (
+    <Router>
+        <Switch>
+            <Route path={routes.HOME} component={Home} />
+        </Switch>
+    </Router>
+);
+
+export default hot(App);

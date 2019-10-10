5c419c97c0b0630031de4f2fb5546ba158fe5708
Jean Ribeiro Alexandre <jean.alexandre.rs@gmail.com>
2019-10-10 08:18:12 -0300

    Installed @material-ui/core.

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const Home = (): JSX.Element => {
-    return <h1>Home</h1>;
-};
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+
+const Home = (): JSX.Element => (
+    <Paper>
+        <Typography variant="h2" component="h1">Home</Typography>
+    </Paper>
+);
 
 export default Home;

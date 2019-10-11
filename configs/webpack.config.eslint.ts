cb9fac4e600768aaeb7718c36fa89459246638b5
Jean Ribeiro Alexandre <jean.alexandre.rs@gmail.com>
2019-10-11 19:47:33 -0300

    Changed module.exports in webpack.config.eslint to export default

--- configs/webpack.config.eslint.ts
+++ configs/webpack.config.eslint.ts
@@ -1,4 +1,6 @@
 /* eslint import/no-unresolved: off, import/no-self-import: off */
 require('@babel/register');
 
-module.exports = require('./webpack.config.renderer.dev').default;
+import DevConfig from './webpack.config.renderer.dev';
+
+export default DevConfig;
